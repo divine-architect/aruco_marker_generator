61253407cdb847c1110499fd233eb71f2eb084e0
divine architect <130902558+divine-architect@users.noreply.github.com>
2023-12-31 23:45:59 +0530

    Create aruco_marker_generation.py

---
+++ aruco_marker_generation.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import argparse as ap
+
+import sys
+import os
+
+# Parse command line arguments
+parser = ap.ArgumentParser()
+parser.add_argument("-i", "--id", type=int, required=True,
+                    help="id of the aruco marker to be generated")
+parser.add_argument("-t", "--type", type=str,
+                    default="DICT_ARUCO_ORIGINAL", help="type of ARUCO marker")
+args = vars(parser.parse_args())
+
+ARUCO_DICT = {
+    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+    "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+    "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+    "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+    "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+arucoDict =cv2.aruco.getPredefinedDictionary(ARUCO_DICT[args['type']])
+tag = np.zeros((300, 300, 1), dtype="uint8")
+cv2.aruco.generateImageMarker(arucoDict, args["id"], 300, tag, 1)
+
+cv2.imwrite('fourth_marker.jpg', tag)
+cv2.imshow("marker", tag)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
